17e83cd09c2d00a7fc4fcb98a1d2b69170492823
Fatima Rahimi <rahimifatima432@gmail.com>
2025-10-28 22:04:38 +0430

    create file axios.interceotro.js

--- src/Constants/axios.interceptro.js
+++ src/Constants/axios.interceptro.js
@@ -1,2 +1,22 @@
 import axios from "axios";
 import { getCookie, setCookie } from "../utils/helpers/Cookie";
+
+const getAccessToken = async () => {
+    const cookie = await getCookie("credential");
+    return cookie?.access_token;
+};
+
+const getRefreshToken = async () => {
+    const cookie = await getCookie("credential");
+    return cookie?.refresh_token;
+};
+
+export const apiClient = axios.create({
+    baseURL: "https://api.escuelajs.co/api/v1",
+    headers: {
+        Authorization: `Bearer ${await getAccessToken()}`,
+    },
+});
+
+
+
