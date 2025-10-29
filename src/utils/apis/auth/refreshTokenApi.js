761a899bf87dc3c6b53d79ab6fd5371a1b4816f4
Fatima Rahimi <rahimifatima432@gmail.com>
2025-10-29 21:46:45 +0430

    create newRefreshToken

--- src/utils/apis/auth/refreshTokenApi.js
+++ src/utils/apis/auth/refreshTokenApi.js
@@ -1,2 +1,11 @@
 import apiClient from "../../../Constants/axios-interceptorjs";
 
+export const refreshTokenApi = async (data) => {
+    try {
+        return await apiClient.post("/auth/refresh_token", data);
+    } catch(error) {
+        return error;
+    }
+};
+
+export default refreshTokenApi;
